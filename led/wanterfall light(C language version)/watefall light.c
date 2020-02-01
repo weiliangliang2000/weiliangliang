3445f19c11562ea3a294cda07e6a436a6dbc1006
Your Name <you@example.com>
2020-02-01 17:03:45 +0800

    waterfall light(C language version)

---
+++ led/wanterfall light(C language version)/watefall light.c
@@ -0,0 +1,22 @@
+#include <STC89C5xRC.H>
+void main()
+{
+	unsigned int i=0;
+	unsigned char z=0;
+	P20=0;
+	P21=1;
+	P22=1;//we set LEDS6 at a low potential.
+	P11=0;//enabled
+	while(1)
+	{
+		P0=~(0X01<<z);
+		for (i=0;i<60000;i++);
+		{
+		  z++;
+		}
+		if(z>=8)
+		{
+			z=0;
+	  }
+  }
+}
